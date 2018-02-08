9b58e2d844622d601b6feb7ed52309b4361c6652
Untitled <john.d.reddock@gmail.com>
2018-02-08 17:16:59 -0500

    JWT

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -23,7 +24,7 @@ var app = express();
 
 //Connection URL
 
-const url = 'mongodb://localhost:27017/confusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 var db = mongoose.connection;
 
@@ -44,34 +45,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false, 
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
 
-function auth(req, res, next){
-  console.log(req.user);
-  if (!req.user){
-        var err = new Error('You are not authenticated!1');
-       res.setHeader('WWW-Authenticate', 'Basic');
-       err.status = 401;
-       next(err);
-  }
-  else {
-        next();
-  }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const authenticate = require('../authenticate');
 const Leaders = require('../models/leaders');
 const leaderRouter = express.Router();
 
@@ -17,7 +17,7 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req,res,next)=> {
+.post(authenticate.verifyUser, (req,res,next)=> {
     Leaders.create(req.body)
     .then((leader) => {
         console.log('Leader created', leader);
@@ -27,11 +27,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));    
 })
-.put((req,res,next)=> {
+.put(authenticate.verifyUser, (req,res,next)=> {
     res.statusCode = 403;
     res.end('PUT operation not supported on /leader');
 })  
-.delete((req,res,next)=> {
+.delete(authenticate.verifyUser, (req,res,next)=> {
     Leaders.remove({})
     .then((resp) => {        
         res.statusCode = 200;
@@ -41,7 +41,7 @@ leaderRouter.route('/')
     .catch((err) => next(err));    
 });
 
-// dishID section
+// leaderID section
 leaderRouter.route('/:leaderId')
 .get((req,res,next)=> {
     Leaders.findById(req.params.leaderId)
@@ -52,11 +52,11 @@ leaderRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req,res,next)=> {
+.post(authenticate.verifyUser, (req,res,next)=> {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/' + req.params.leaderId);
 })
-.put((req,res,next)=> {
+.put(authenticate.verifyUser, (req,res,next)=> {
     Leaders.findByIdAndUpdates(req.params.leaderId, {
         $set: req.body
     }, { new: true })
@@ -67,7 +67,7 @@ leaderRouter.route('/:leaderId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req,res,next)=> {
+.delete(authenticate.verifyUser, (req,res,next)=> {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((resp)=> {
         res.statusCode = 200;
